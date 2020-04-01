2a75d25e40b8d39f6dcf50e129116e77ef4d2864
17021084 <dotrunghg1999@gmail.com>
2020-04-01 16:51:09 +0700

    first commit

---
+++ sampleData.js
@@ -0,0 +1,12 @@
+const sampleData= [
+    { id:1 , unitName : 'lession 1'  },
+    { id:2 , unitName : 'lession 2 '  },
+    { id:3 , unitName : 'lession 3'  },
+    { id:4 , unitName : 'lession 4'  },
+    { id:5 , unitName : 'lession 5'  },
+    { id:6 , unitName : 'lession 6'  },
+    { id:7 , unitName : 'lession 7'  },
+    { id:8 , unitName : 'lession 8'  }
+];
+
+export default sampleData;
